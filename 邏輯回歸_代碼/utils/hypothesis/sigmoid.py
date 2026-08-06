7410ca260933e6cc7efbc0fd9e7fcfaa30e8f1eb
jingwenliu0113-afk <jingwenliu0113@gmail.com>
2026-08-06 11:34:10 +0800

    Remove mistakenly uploaded files

--- 邏輯回歸_代碼/utils/hypothesis/sigmoid.py
+++
@@ -1,9 +0,0 @@
-"""Sigmoid function"""
-
-import numpy as np
-
-
-def sigmoid(matrix):
-    """Applies sigmoid function to NumPy matrix"""
-    matrix = np.clip(matrix, -500, 500)
-    return 1 / (1 + np.exp(-matrix))
